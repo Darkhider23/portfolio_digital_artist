a29b25c4852cbc190d2a7f9ea8a7b1614f98196f
Farcas Alex <farchicoding@gmail.com>
2024-08-10 23:18:27 +0300

    first-card

---
+++ portfolio-frontend/src/Icons.tsx
@@ -0,0 +1,25 @@
+// // Import specific icons from the react-icons library
+// import { FaHome, FaUser, FaCog } from 'react-icons/fa';
+// import { MdFavorite, MdSettings } from 'react-icons/md';
+// import { AiFillHeart, AiOutlineSearch } from 'react-icons/ai';
+
+// // Define a type for icon components
+// export type IconType = React.ComponentType<React.SVGProps<SVGSVGElement>>;
+
+// // Export the icons as named exports with type annotations
+// export const HomeIcon: IconType = FaHome;
+// export const UserIcon: IconType = FaUser;
+// export const SettingsIcon: IconType = FaCog;
+// export const FavoriteIcon: IconType = MdFavorite;
+// export const HeartIcon: IconType = AiFillHeart;
+// export const SearchIcon: IconType = AiOutlineSearch;
+
+// // Optionally, you can export all icons as a single object if needed
+// export const Icons = {
+//   HomeIcon,
+//   UserIcon,
+//   SettingsIcon,
+//   FavoriteIcon,
+//   HeartIcon,
+//   SearchIcon,
+// };
--- portfolio-frontend/src/components/WorkList.tsx
+++ portfolio-frontend/src/components/WorkList.tsx
@@ -2,19 +2,20 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import WorkItem from './WorkItem';
+import './WorkList.css'
 
 const WorkList: React.FC = () => {
   const [works, setWorks] = useState([]);
 
   // Access environment variable
-const baseURL = process.env.REACT_APP_API_BASE_URL;
-// const baseURL = 'http://localhost:3000';
+  const baseURL = process.env.REACT_APP_API_BASE_URL;
+  // const baseURL = 'http://localhost:3000';
 
-console.log(baseURL)
-// Create an Axios instance with the base URL
-const api = axios.create({
-  baseURL,
-});
+  console.log(baseURL)
+  // Create an Axios instance with the base URL
+  const api = axios.create({
+    baseURL,
+  });
 
   useEffect(() => {
     api.get('/works').then(response => {
@@ -28,14 +29,33 @@ const api = axios.create({
     });
   };
 
+  const[showDescription, setShowDescription] = useState(false);
+
+  const toggleDescription = () => {
+    setShowDescription(!showDescription);
+  };
+
   return (
     <div>
       <h1>Portfolio</h1>
-      <ul>
+      {/* <ul>
         {works.map((work: any) => (
           <WorkItem key={work.id} work={work} onDelete={handleDelete} />
         ))}
-      </ul>
+      </ul> */}
+      <div className="card-container">
+        <div className="card-image"
+        style={{backgroundImage: `url('/work_images/image1.jpg')`}}>
+          <button className="show-description-button" onClick={toggleDescription}>
+            {showDescription ? 'Hide Description' : 'Show Description'}
+          </button>
+          <div className={`card-description ${showDescription ? 'show' : ''}`}>
+            <h2 className="card-title">Card Title 1</h2>
+            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 };
